f351347b96c4bdda1234ede1292b48c3786a3c43
Nikita Sychou <nikita.sychou256@gmail.com>
2026-05-19 22:04:40 +0100

    Add Swagger OpenAPI support

--- src/InventoryService/LogisticsHub.InventoryService/Program.cs
+++ src/InventoryService/LogisticsHub.InventoryService/Program.cs
@@ -6,8 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbInfrastructure(builder.Configuration);
 builder.Services.AddHealthChecks();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "LogisticsHub Inventory API v1");
+    });
+}
 
 app.MapHealthChecks(HealthEndpointPath);
 
