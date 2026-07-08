23e73e5a5ae9b66aef864c665931e55f6f30e2ff
Nikita Sychou <nikita.sychou256@gmail.com>
2026-07-08 06:22:29 +0100

    Add Angular auth route guards

--- src/Web/LogisticsHub.Web/src/app/app.routes.ts
+++ src/Web/LogisticsHub.Web/src/app/app.routes.ts
@@ -1,16 +1,25 @@
 import { Routes } from '@angular/router';
+import { signedInGuard, signedOutGuard } from './core/auth/signed-in.guard';
 
 export const routes: Routes = [
   {
+    path: 'sign-in',
+    canActivate: [signedOutGuard],
+    loadComponent: () => import('./core/auth/sign-in-page').then((component) => component.SignInPage),
+  },
+  {
     path: 'companies',
+    canActivate: [signedInGuard],
     loadChildren: () => import('./features/companies/companies.routes').then((routes) => routes.companiesRoutes),
   },
   {
     path: 'inventory',
+    canActivate: [signedInGuard],
     loadChildren: () => import('./features/inventory/inventory.routes').then((routes) => routes.inventoryRoutes),
   },
   {
     path: 'shipments',
+    canActivate: [signedInGuard],
     loadChildren: () => import('./features/shipments/shipments.routes').then((routes) => routes.shipmentsRoutes),
   },
   {
