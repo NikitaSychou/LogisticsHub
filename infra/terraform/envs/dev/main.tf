b8e1e0435c73cf4b9eabd8f1160738d964fdd84f
Nikita Sychou <nikita.sychou256@gmail.com>
2026-07-13 08:31:03 +0100

    Format Terraform foundation files

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -102,17 +102,17 @@ resource "azurerm_key_vault" "main" {
 }
 
 resource "azurerm_storage_account" "frontend" {
-  name                             = var.frontend_storage_account_name
-  resource_group_name              = azurerm_resource_group.main.name
-  location                         = azurerm_resource_group.main.location
-  account_tier                     = "Standard"
-  account_replication_type         = "LRS"
-  account_kind                     = "StorageV2"
-  min_tls_version                  = "TLS1_2"
-  allow_nested_items_to_be_public  = false
-  shared_access_key_enabled        = true
+  name                              = var.frontend_storage_account_name
+  resource_group_name               = azurerm_resource_group.main.name
+  location                          = azurerm_resource_group.main.location
+  account_tier                      = "Standard"
+  account_replication_type          = "LRS"
+  account_kind                      = "StorageV2"
+  min_tls_version                   = "TLS1_2"
+  allow_nested_items_to_be_public   = false
+  shared_access_key_enabled         = true
   infrastructure_encryption_enabled = true
-  tags                             = var.tags
+  tags                              = var.tags
 }
 
 resource "azurerm_storage_account_static_website" "frontend" {
