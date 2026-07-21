d02a2f838d960f81e265ba22f479bda73d1486c6
Nikita Sychou <nikita.sychou256@gmail.com>
2026-07-21 17:58:50 +0100

    Grant AKS AcrPull access

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -60,6 +60,11 @@ resource "azurerm_container_registry" "main" {
   tags                = var.tags
 }
 
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
 resource "azurerm_kubernetes_cluster" "main" {
   name                      = var.aks_name
   resource_group_name       = azurerm_resource_group.main.name
